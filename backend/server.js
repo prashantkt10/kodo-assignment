9b7bcdc24a4b6fcfd40c094f645da890304964f7
Prashant <prashantkt10@gmail.com>
2021-12-12 12:48:43 +0530

    cors not needed

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,8 @@
 require('module-alias/register');
 const express = require("express");
-const cors = require("cors");
 const getFeedRouter = require("./routes/feed");
 const healthRouter = require("./routes/health");
 const app = express();
-
-app.use(cors());
 
 app.use("/feed", getFeedRouter);
 app.use("/health", healthRouter);
